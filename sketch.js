bc16f1a1d1c06ac3d0871894a710fbf9dbbad61f
jacquelinelouie <jacquelinelouie@users.noreply.github.com>
2020-09-29 16:32:25 -0700

    p5 import

--- sketch.js
+++ sketch.js
@@ -1,14 +1,7 @@
-// food I love
-let food = ["sushi", "friedchicken", "noodles", "soup", "fries"];
-
 function setup() {
-  createCanvas (600,600);
-  background (200);
-
-  console.log(food[0]);
+  createCanvas(600,600);
 }
 
-function draw () {
-
-
+function draw() {
+  background(200);
 }
