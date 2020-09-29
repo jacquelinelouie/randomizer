ddfa0752ab330af4bf389cc0e582775b2b869986
jacquelinelouie <jacquelinelouie@users.noreply.github.com>
2020-09-29 13:37:04 -0700

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+// food I love
+let food = ["sushi", "friedchicken", "noodles", "soup", "fries"];
+
+function steup () {
+  creatCanvas (600,600);
+  background (200);
+
+  console.log(food[0]);
+}
+
+function draw () {
+
+
+}
