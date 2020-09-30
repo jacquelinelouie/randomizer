1c73eb91dbec875e5be4ec01b38a8d6df2cdc5ca
jacquelinelouie <jacquelinelouie@users.noreply.github.com>
2020-09-29 18:23:40 -0700

    randomizer tutorials

--- sketch.js
+++ sketch.js
@@ -1,7 +1,39 @@
+// food I love
+
+let food = [{
+  kind: "sashimi"
+}, {
+  name: "friedchicken",
+  kind: "popeyes"
+}, {
+  name: "noodles",
+  kind: "pasta"
+}, {
+  name: "soup",
+  kind: "tomato"
+}];
+
+let randomIndex;
+
+
 function setup() {
-  createCanvas(600,600);
+  createCanvas (600,600);
+  background (200);
+
+  mic = new p5.AudioIn()
+  mic.start();
+  mic.stop();
+
 }
 
-function draw() {
-  background(200);
+function draw () {
+
+
+}
+
+function mousePressed() {
+  background(random(200, 255));
+  randomIndex = int(random(food.length));
+  text(food[randomIndex].name, 50,50);
+  food.splice(randomIndex, 1);
 }
